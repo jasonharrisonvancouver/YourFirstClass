73c6789163d135d39a10c258149893c4914940b3
Jason Harrison <jasonharrisonvancouver@gmail.com>
2018-12-31 15:41:22 -0800

    Initial Commit

---
+++ YourFirstClass/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  YourFirstClass
+//
+//  Created by jason harrison on 2018-12-31.
+//  Copyright © 2018 jason harrison. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
